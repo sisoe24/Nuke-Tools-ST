ec2f1797eb67f99a346745028c7507c8b1cc72d0
virgilsisoe <virgilsisoe@gmail.com>
2021-10-21 19:08:28 -0400

    new package fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 """Configuration file for pytest."""
 
+import os
 import pytest
 
 from src import main
+
+
+@pytest.fixture(scope='session')
+def package():
+    """Package directory path."""
+    current_dir = os.path.dirname(__file__)
+    package_dir = os.path.abspath(os.path.dirname(current_dir))
+    yield package_dir
 
 
 @pytest.fixture(autouse=True)
